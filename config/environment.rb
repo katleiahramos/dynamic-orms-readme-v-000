9fd8761aad3e7cc138b5f899d4369aefd1bf725b
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-11 15:46:26 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
 require 'sqlite3'
 
-#create SQL database 
+#create SQL database
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
 
-#clear any songs table from that database before starting 
+#clear any songs table from that database before starting
 DB[:conn].execute("DROP TABLE IF EXISTS songs")
 
-#cretae the table if it doesn't exist with given columns 
+#cretae the table if it doesn't exist with given columns
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
   id INTEGER PRIMARY KEY,
@@ -15,6 +15,8 @@ sql = <<-SQL
   )
 SQL
 
-#execute the sql 
+#execute the sql
 DB[:conn].execute(sql)
+
+#return database row as a has with the column names as keys
 DB[:conn].results_as_hash = true
